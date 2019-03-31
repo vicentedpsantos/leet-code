19951cc4de7010943fb409587065482225bf491d
Vicente Santos <vicentedpsantos@gmail.com>
2019-03-31 19:01:51 -0300

    more easy challenges

---
+++ easy_n_repeated_elements_in_size_2n_array.rb
@@ -0,0 +1,15 @@
+# In a array A of size 2N, there are N+1 unique elements, and exactly one of these elements is repeated N times.
+#
+# Return the element repeated N times.
+
+def repeated_n_times(a)
+    n = a.length / 2
+
+    a.length.times do |position|
+        return a[position] if a.count(a[position]) == n
+    end
+end
+
+repeated_n_times([1,2,3,3])
+repeated_n_times([2,1,2,5,3,2])
+repeated_n_times([5,1,5,2,5,3,5,4])
---
+++ easy_two_sum.rb
@@ -0,0 +1,14 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+def two_sum(nums, target)
+    nums.length.times do |i|
+        nums.length.times do |j|
+            return [i, j] if nums[i] + nums[j] == target && i != j
+        end
+    end
+end
+
+two_sum([2, 7, 11, 15], 9)
+two_sum([2, 1, 6, 3], 4)
