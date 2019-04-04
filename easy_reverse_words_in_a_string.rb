d67bb22ba57db85731ed8738677f0a331b388b0d
Vicente Santos <vicentedpsantos@gmail.com>
2019-04-03 21:05:21 -0300

    Completed two more challenges

---
+++ easy_reverse_words_in_a_string.rb
@@ -0,0 +1,10 @@
+# Given a string, you need to reverse the order of characters in each word within
+# a sentence while still preserving whitespace and initial word order.
+
+def reverse_words(s)
+    arr = s.split(" ").map { |word| word.reverse! }
+    arr.join(' ')
+end
+
+reverse_words('Let\'s fart this place up')
+reverse_words('A random string to be tested!')
