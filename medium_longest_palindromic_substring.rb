f34619730714bad8a9e98e0a4396c38133bcecec
Vicente Santos <vicentedpsantos@gmail.com>
2019-04-22 11:13:24 -0300

    Added medium challenge that works but doesnt pass time limit

---
+++ medium_longest_palindromic_substring.rb
@@ -0,0 +1,30 @@
+# Given a string s, find the longest palindromic substring in s. You may assume
+# that the maximum length of s is 1000.
+#
+# Example 1:
+#
+# Input: "babad"
+# Output: "bab"
+# Note: "aba" is also a valid answer.
+#
+# Example 2:
+#
+# Input: "cbbd"
+# Output: "bb"
+
+def longest_palindrome(s)
+  return '' if s == ''
+  arr = s.chars
+
+  s.length.downto(1) do |char|
+    palindrome = arr.each_cons(char).find { |tested| tested == tested.reverse }
+    return palindrome.join if palindrome
+  end
+end
+
+p longest_palindrome('ac')
+p longest_palindrome('a')
+p longest_palindrome('babad') #=> "bab"
+p longest_palindrome('cbbd') # "bb"
+p longest_palindrome('')
+p longest_palindrome('iopsajhffgvrnyitusobwcxgwlwniqchfnssqttdrnqqcsrigjsxkzcmuoiyxzerakhmexuyeuhjfobrmkoqdljrlojjjysfdslyvckxhuleagmxnzvikfitmkfhevfesnwltekstsueefbrddxrmxokpaxsenwlgytdaexgfwtneurhxvjvpsliepgvspdchmhggybwupiqaqlhjjrildjuewkdxbcpsbjtsevkppvgilrlspejqvzpfeorjmrbdppovvpzxcytscycgwsbnmspihzldjdgilnrlmhaswqaqbecmaocesnpqaotamwofyyfsbmxidowusogmylhlhxftnrmhtnnljjhhcfvywsqimqxqobfsageysonuoagmmviozeouutsiecitrmkypwknorjjiaasxfhsftypspwhvqovmwkjuehujofiabznpipidhfxpoustquzyfurkcgmioxacleqdxgrxbldcuxzgbcazgfismcgmgtjuwchymkzoiqhzaqrtiykdkydgvuaqkllbsactntexcybbjaxlfhyvbxieelstduqzfkoceqzgncvexklahxjnvtyqcjtbfanzgpdmucjlqpiolklmjxnscjcyiybdkgitxnuvtmoypcdldrvalxcxalpwumfx')
